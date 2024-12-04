7f2a23c90c29d7e832a5032629298c0f257126bd
md <marceldiomare@gmail.com>
2024-12-05 02:10:46 +0300

    added admin.action def clear_debt

--- network/admin.py
+++ network/admin.py
@@ -8,3 +8,10 @@ class NetworkNodeAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'city', 'country')
     ordering = ('created_at',)
 
+    # Действие для очистки задолженности
+    actions = ['clear_debt']
+
+    @admin.action(description="Очистить задолженность перед поставщиком")
+    def clear_debt(self, request, queryset):
+        queryset.update(debt=0)
+        self.message_user(request, "Задолженность успешно очищена для выбранных объектов.")
