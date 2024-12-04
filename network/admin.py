7d0e4fdc913ef990c37064c0da0bab7f5aef9a64
md <marceldiomare@gmail.com>
2024-12-05 01:54:59 +0300

    configured the display of the admin panel, create superuser

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import NetworkNode
 
-# Register your models here.
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'city', 'supplier', 'debt', 'created_at')
+    list_filter = ('country', 'city')
+    search_fields = ('name', 'email', 'city', 'country')
+    ordering = ('created_at',)
+
