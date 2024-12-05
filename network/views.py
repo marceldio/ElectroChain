736df051b51c3799a4e9e7238cabc35e459a52a0
md <marceldiomare@gmail.com>
2024-12-06 02:56:51 +0300

    GET http://127.0.0.1:8000/api/network/ is ready

--- network/views.py
+++ network/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import NetworkNode
+from .serializers import NetworkNodeSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 
-# Create your views here.
+
+class NetworkNodeViewSet(viewsets.ModelViewSet):
+    """
+    CRUD для модели NetworkNode.
+    Поле 'debt' доступно только для чтения.
+    """
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
