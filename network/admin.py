62477badcd23ca44ecbd4bdc75d45aa462146295
md <marceldiomare@gmail.com>
2024-12-08 22:44:55 +0300

    In the admin panel on the network node page I added clear debt

--- network/admin.py
+++ network/admin.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
 from .models import NetworkNode, Contact, Product
+from django.contrib import admin
+from django.urls import reverse, path
+from django.utils.html import format_html
 from .forms import NetworkNodeAdminForm
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 @admin.register(Contact)
@@ -20,37 +24,67 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
     form = NetworkNodeAdminForm
-    list_display = ['name', 'supplier', 'contact', 'debt', 'created_at']
-    list_display_links = ['supplier']
+    list_display = ['name', 'supplier_link', 'contact', 'debt', 'created_at']
     search_fields = ['name', 'supplier__name', 'contact__email']
     ordering = ['created_at']
-    list_filter = ('contact__country', 'contact__city',)
+    list_filter = ('contact__country', 'contact__city',)  # Фильтр по стране, городу
+    readonly_fields = ['supplier_link', 'clear_debt_button']  # Поля для ссылки на поставщика и действия
+    actions = ['clear_debt']  # Вернем действие на страницу списка
 
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        if db_field.name == "products":
-            supplier_id = request.GET.get('supplier')
-            if supplier_id:  # Если поставщик выбран
-                kwargs["queryset"] = Product.objects.filter(network_nodes=supplier_id)
-            else:  # Если поставщик не выбран, показываем все продукты
-                kwargs["queryset"] = Product.objects.all()
-        return super().formfield_for_manytomany(db_field, request, **kwargs)
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
 
-    # Действие для очистки задолженности
-    actions = ['clear_debt']
+        # Добавляем динамическую фильтрацию для контактов
+        if 'contact' in form.base_fields:
+            form.base_fields['contact'].queryset = Contact.objects.all()
+
+        return form
+
+    def supplier_link(self, obj):
+        """Кликабельная ссылка на страницу поставщика."""
+        if obj.supplier:
+            url = reverse('admin:network_networknode_change', args=[obj.supplier.id])
+            return format_html('<a href="{}">{}</a>', url, obj.supplier.name)
+        return "No Supplier"
+    supplier_link.short_description = "Supplier"  # Название колонки
+
+    def clear_debt_button(self, obj):
+        """Кнопка для очистки задолженности на странице изменения."""
+        if obj and obj.debt > 0:
+            return format_html(
+                '<a class="button" href="{}">Очистить задолженность</a>',
+                reverse('admin:network_networknode_clear_debt', args=[obj.pk])
+            )
+        return "Нет задолженности"
+    clear_debt_button.short_description = "Очистить задолженность"
+
+    def get_urls(self):
+        """Добавляем кастомный URL для кнопки очистки задолженности."""
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                '<int:pk>/clear-debt/',
+                self.admin_site.admin_view(self.clear_debt_action),
+                name='network_networknode_clear_debt',
+            ),
+        ]
+        return custom_urls + urls
+
+    def clear_debt_action(self, request, pk, *args, **kwargs):
+        """Обработка действия очистки задолженности."""
+        obj = self.get_object(request, pk)
+        if obj:
+            obj.debt = 0
+            obj.save()
+            self.message_user(request, "Задолженность успешно очищена.", messages.SUCCESS)
+        return redirect('admin:network_networknode_change', obj.pk)
 
     @admin.action(description="Очистить задолженность перед поставщиком")
     def clear_debt(self, request, queryset):
+        """Admin Action для очистки задолженности на странице списка."""
         queryset.update(debt=0)
         self.message_user(request, "Задолженность успешно очищена для выбранных объектов.")
 
-    def get_supplier(self, obj):
-        return obj.supplier.name if obj.supplier else 'Нет поставщика'
-    get_supplier.short_description = 'Поставщик'
-
-    def get_country(self, obj):
-        return obj.contact.country if obj.contact else 'Нет данных'
-    get_country.short_description = 'Страна'
-
-    def get_city(self, obj):
-        return obj.contact.city if obj.contact else 'Нет данных'
-    get_city.short_description = 'Город'
+    class Media:
+        """Подключение пользовательских скриптов, если необходимо."""
+        js = ('admin/js/clear_debt.js',)
